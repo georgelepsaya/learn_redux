a0c857f7149ac77d71c5f2c6bd9413b4564a216d
georgelepsaya <georgelepsaya@gmail.com>
2022-05-14 19:38:03 +0400

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { useSelector } from "react-redux";
+import { CartIcon } from "../icons";
+
+function withSelector(Component) {
+  return (props) => (
+    <Component {...props} amount={useSelector((store) => store.cart.amount)} />
+  );
+}
+
+class Navbar extends Component {
+  render() {
+    const amount = this.props.amount;
+    return (
+      <nav>
+        <div className="nav-center">
+          <h3>redux toolkit</h3>
+          <div className="nav-container">
+            <CartIcon />
+            <div className="amount-container">
+              <p className="total-amount">{amount}</p>
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default withSelector(Navbar);
